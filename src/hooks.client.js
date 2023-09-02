869e376f8293046223825aef7e1d4ba0c5064e6c
jeromebandril <102626866+jeromebandril@users.noreply.github.com>
2023-09-02 15:42:04 +0200

    bible preloading moved on component mount

--- src/hooks.client.js
+++ src/hooks.client.js
@@ -1,18 +1,16 @@
-import { invoke } from '@tauri-apps/api/tauri';
-import { bible } from '../src/store';
+// import { invoke } from '@tauri-apps/api/tauri';
+// import { bible } from '../src/store';
 
-const resourcePath = '\\smyrna.bible.v2\\src-tauri\\assets\\data\\kjv-english.json';
+// const resourcePath = '\\smyrna.bible.v2\\src-tauri\\assets\\data\\kjv-english.json';
 
-invoke('read_bible_source', { filePath: resourcePath })
-  .then((content) => {
-    // temp solution parsing with js instead of callind commnad
-    let kjvBible = JSON.parse(content);
-    // doesn't work fix struct Data
-    // invoke('load_bible', { content: content })
-    //   .then((parsedBible) => (kjvBible = parsedBible))
-    //   .catch((error) => console.log(error));
-    console.log('ciaone');
-    bible.set(kjvBible);
-    kjvBible = null;
-  })
-  .catch((error) => console.log(error));
+// invoke('read_bible_source', { filePath: resourcePath })
+//   .then((content) => {
+//     // temp solution parsing with js instead of callind commnad
+//     let kjvBible = JSON.parse(content);
+//     // doesn't work fix struct Data
+//     // invoke('load_bible', { content: content })
+//     //   .then((parsedBible) => (kjvBible = parsedBible))
+//     //   .catch((error) => console.log(error));
+//     bible.set(kjvBible);
+//   })
+//   .catch((error) => console.log(error));
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,26 @@
 <script>
+  import { bible } from '../store';
+  import { invoke } from '@tauri-apps/api/tauri';
+  import {onMount} from 'svelte'
 	import Bibleview from '$lib/Bibleview.svelte';
-  import Toolbar from '../lib/Toolbar.svelte';
+  import Toolbar from '$lib/Toolbar.svelte';
 	import './styles.css';
+
+  const resourcePath = '\\smyrna.bible.v2\\src-tauri\\assets\\data\\kjv-english.json';
+  onMount(()=>{
+
+  invoke('read_bible_source', { filePath: resourcePath })
+    .then((content) => {
+      // temp solution parsing with js instead of callind commnad
+      let kjvBible = JSON.parse(content);
+      // doesn't work fix struct Data
+      // invoke('load_bible', { content: content })
+      //   .then((parsedBible) => (kjvBible = parsedBible))
+      //   .catch((error) => console.log(error));
+      bible.set(kjvBible);
+    })
+    .catch((error) => console.log(error));
+  })
 </script>
 
 <svelte:head>
