a8159843a5bd5e71385f6d8bc3ef2fc2d889820d
jeromebandril <102626866+jeromebandril@users.noreply.github.com>
2023-09-02 11:39:17 +0200

    add store.js for storing reusable variables

--- src/hooks.client.js
+++ src/hooks.client.js
@@ -1,16 +1,17 @@
 import { invoke } from '@tauri-apps/api/tauri';
+import { bible } from '../src/store';
 
 const resourcePath = '\\smyrna.bible.v2\\src-tauri\\assets\\data\\kjv-english.json';
-
-let kjvBible;
 
 invoke('read_bible_source', { filePath: resourcePath })
   .then((content) => {
     // temp solution parsing with js instead of callind commnad
-    kjvBible = JSON.parse(content);
+    let kjvBible = JSON.parse(content);
     // doesn't work fix struct Data
     // invoke('load_bible', { content: content })
     //   .then((parsedBible) => (kjvBible = parsedBible))
     //   .catch((error) => console.log(error));
+    bible.set(kjvBible);
+    kjvBible = null;
   })
   .catch((error) => console.log(error));
