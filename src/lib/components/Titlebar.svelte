2e72510f8dd5af275d3236bd9922486fd4edea54
jeromebandril <102626866+jeromebandril@users.noreply.github.com>
2023-09-11 15:31:43 +0200

    add buttons icons

--- src/lib/components/Titlebar.svelte
+++ src/lib/components/Titlebar.svelte
@@ -1,7 +1,16 @@
 <script lang="ts">
-  import { appWindow  } from "@tauri-apps/api/window";
+  import { appWindow } from "@tauri-apps/api/window";
   import { isDarkMode } from "../../store";
   import smyrnaIcon from "$lib/images/32x32.png"
+  import { onMount } from "svelte";
+
+
+  let isMaximized: boolean;
+  onMount(async () => {
+    isMaximized = await appWindow.isMaximized();
+    console.log(isMaximized);
+  });
+
 </script>
 
 <!-- svelte-ignore a11y-click-events-have-key-events -->
@@ -12,21 +21,19 @@
     <img src={smyrnaIcon} height={16} alt="icon">
     <span>Smyrna Bible</span>
   </div>
-  <div class="titlebar-buttons">
+  <div class="titlebar-buttons" class:darkmode={$isDarkMode}>
     <div on:click={appWindow.minimize} id="titlebar-minimize">
-      <img
-        src="https://api.iconify.design/mdi:window-minimize.svg"
-        alt="minimize"
-      />
+      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16"><path fill="currentColor" d="M14 8v1H3V8h11z"/></svg>
     </div>
     <div on:click={appWindow.toggleMaximize} id="titlebar-maximize">
-      <img
-        src="https://api.iconify.design/mdi:window-maximize.svg"
-        alt="maximize"
-      />
+        {#if isMaximized}
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16"><g fill="currentColor"><path d="M3 5v9h9V5H3zm8 8H4V6h7v7z"/><path fill-rule="evenodd" d="M5 5h1V4h7v7h-1v1h2V3H5v2z" clip-rule="evenodd"/></g></svg>
+        {:else}
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16"><path fill="currentColor" d="M3 3v10h10V3H3zm9 9H4V4h8v8z"/></svg>
+        {/if}
     </div>
     <div on:click={appWindow.close} id="titlebar-close">
-      <img src="https://api.iconify.design/mdi:close.svg" alt="close" />
+      <svg xmlns="http://www.w3.org/2000/svg" stroke-width="1" width="16" height="16" viewBox="0 0 16 16"><path fill="currentColor" fill-rule="evenodd" d="m7.116 8l-4.558 4.558l.884.884L8 8.884l4.558 4.558l.884-.884L8.884 8l4.558-4.558l-.884-.884L8 7.116L3.442 2.558l-.884.884L7.116 8z" clip-rule="evenodd"/></svg>
     </div>
   </div>
 </div>
@@ -73,12 +80,17 @@
   }
 
   .titlebar-buttons > div:hover {
-    transition: 300ms;
+    transition: 150ms;
     background: lightgray;
   }
 
+  .titlebar-buttons.darkmode > div:hover {
+    background: darkslategray
+  }
+
   #titlebar-close:hover {
-    transition: 300ms;
+    transition: 150ms;
+    color: white;
     background: red;
   }
 </style>
