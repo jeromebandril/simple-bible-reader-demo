34453d385d78842a36f99af70f2c542154867fb8
jeromebandril <102626866+jeromebandril@users.noreply.github.com>
2023-09-19 01:20:23 +0200

    add: remove titlebar when fullscreen

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,14 +2,15 @@
   import Titlebar from '$lib/components/Titlebar.svelte';
   import Toolbar from '$lib/components/Toolbar/Toolbar.svelte';
   import Display from '$lib/components/Display/Display.svelte';
+  import { isFullscreen } from '../store';
 	import './styles.css';
-
-
 </script>
 
 <svelte:document on:contextmenu|preventDefault></svelte:document>
 <main>
-  <Titlebar/>
+  {#if !$isFullscreen}
+    <Titlebar/>
+  {/if}
   <Toolbar/>
   <Display/>
 </main>
